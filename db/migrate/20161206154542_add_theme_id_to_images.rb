0151c8d53423c4ccffe190db592dee45c8543c68
Ubuntu <ubuntu@experteese2>
2016-12-20 18:24:22 +0000

    Eight practice

--- db/migrate/20161206154542_add_theme_id_to_images.rb
+++ db/migrate/20161206154542_add_theme_id_to_images.rb
@@ -1,5 +1,8 @@
 class AddThemeIdToImages < ActiveRecord::Migration[5.0]
-  def change
+  def up
     add_column :images, :theme_id, :integer
+  end
+  def down
+    remove_column :images, :theme_id
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161206155026) do
+ActiveRecord::Schema.define(version: 20161220181441) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,9 +34,13 @@ ActiveRecord::Schema.define(version: 20161206155026) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
     t.integer  "age"
+    t.string   "password_digest"
+    t.string   "remember_token"
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["remember_token"], name: "index_users_on_remember_token", using: :btree
   end
 
   create_table "values", force: :cascade do |t|
